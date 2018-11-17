30dd4abcd6df1475b3548e735ca12b2f1ccb33bc
Kiley Bobbitt <jkileybob@aol.com>
2018-11-17 19:22:25 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,36 +7,3 @@ def nyc_pigeon_organizer(data)
       end
     end
   end
-
-  data[:color].each do | color, name_array |
-      name_array.each do | name |
-        pigeon_list.keys.each do | key |
-          if name == key
-            pigeon_list[name][:color] << color.to_s
-          end
-        end
-      end
-  end
-
-  data[:gender].each do | gender, name_array |
-      name_array.each do | name |
-        pigeon_list.keys.each do | key |
-          if name == key
-            pigeon_list[name][:gender] << gender.to_s
-          end
-        end
-      end
-  end
-
-  data[:lives].each do | location, name_array |
-      name_array.each do | name |
-        pigeon_list.keys.each do | key |
-          if name == key
-            pigeon_list[name][:lives] << location.to_s
-          end
-        end
-      end
-  end
-
-  return pigeon_list
-end
